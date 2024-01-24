78f39dbf7a3ef97f2a19ed9044af2c07d7d132cf
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2024-01-24 19:25:21 +0330

    fixed guid editor not working properly

--- Assets/Plugins/MoonSharp/Extra/Editor/GUIDEditor.cs
+++ Assets/Plugins/MoonSharp/Extra/Editor/GUIDEditor.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
+using System;
+using System.Runtime.InteropServices;
 [CustomEditor(typeof(UnityLua.GUID))]
 public class GUIDEditor : Editor
 {
     private SerializedProperty type;
+    UnityLua.GUID guidObject;
     public override void OnInspectorGUI()
     {
-        
         serializedObject.Update();
-        SerializedProperty guid = serializedObject.FindProperty("guid");
-        SerializedProperty update = serializedObject.FindProperty("update");
-        GUILayout.TextField($"GUID: {guid.stringValue}");
-        if (GUILayout.Button("Update"))
-            update.boolValue = true;
+        guidObject = (UnityLua.GUID)target;
+        GUILayout.TextField($"GUID: {guidObject.guid}");
+        if (GUILayout.Button("Update")) {
+            update_guid();
+        }
+    }
+    private void update_guid() {
+        try {
+            string asset_path = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(guidObject);
+            guidObject.guid = AssetDatabase.AssetPathToGUID(asset_path);
+            PrefabUtility.ApplyPrefabInstance(guidObject.gameObject, InteractionMode.AutomatedAction);
+        } catch (System.Exception) {
+            if (string.IsNullOrEmpty(guidObject.guid)) guidObject.guid = Guid.NewGuid().ToString();
+        }
     }
 }
--- Assets/Plugins/MoonSharp/Extra/GUID.cs
+++ Assets/Plugins/MoonSharp/Extra/GUID.cs
@@ -1,31 +1,6 @@
 using UnityEngine;
-using System;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 namespace UnityLua {
-#if UNITY_EDITOR
-[ExecuteInEditMode]
-#endif
     public class GUID : MonoBehaviour {
         public string guid;
-#if UNITY_EDITOR
-        public bool update = false;
-        private void Update() {
-            if (!update) {
-                return;
-            }
-            try {
-                string asset_path = PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(gameObject);
-                guid = AssetDatabase.AssetPathToGUID(asset_path);
-                update = false;
-                PrefabUtility.ApplyPrefabInstance(gameObject, InteractionMode.AutomatedAction);
-            } catch (System.Exception) {
-                if (string.IsNullOrEmpty(guid)) guid = Guid.NewGuid().ToString();
-                update = false;
-            }
-        }
-#endif
     }
 }
